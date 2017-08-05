288baf7e5b2cda1865fb5b35ed1d3dec7624c04f
David Cady Jr <davidcadyjr@mac.com>
2017-08-05 15:30:58 -0500

    got projects div to pin

--- js/main.js
+++ js/main.js
@@ -2,34 +2,51 @@ $(document).ready(function(){
 
 	var controller = new ScrollMagic.Controller();
 
-	// var pinIntro = new ScrollMagic.Scene({
+	var wipeAnimation = new TimelineMax()
+		.fromTo('#project1', 1, {x: '-100%'}, {x: "0%", ease: Linear.easeNone})
+		.fromTo('#project2', 1, {x: '-100%'}, {x: "0%", ease: Linear.easeNone})
+		.fromTo('#project3', 1, {x: '-100%'}, {x: "0%", ease: Linear.easeNone});
 
+
+	// var firstScene = new ScrollMagic.Scene({
 	// 	triggerElement: '.intro',
-	// 	triggerHook: 0.4,
-	// 	duration: '90%'
+	// 	duration: '10%',
+	// 	triggerHook: "onEnter"
 	// })
+	// .setClassToggle('.projects', 'fade-in')
 	// .addIndicators({
-	// 	name: 'start pin',
-	// 	colorTrigger: 'red',
+	// 	name: 'fade scene 1',
+	// 	colorTrigger: 'black',
 	// 	colorStart: '#75C695',
-	// 	colorEnd: 'green'
+	// 	colorEnd: 'pink'
 	// })
-	// .setPin('.intro', {pushFollowers: false})
 	// .addTo(controller);
 
-	var projectScene = new ScrollMagic.Scene({
-		triggerElement: '.intro',
-		duration: '30%',
-		triggerHook: 0.1
-	})
-	.setClassToggle('.projects', 'fade-in')
-	.addIndicators({
-		name: 'fade scene',
-		colorTrigger: 'black',
-		colorStart: '#75C695',
-		colorEnd: 'pink'
-	})
 
+	// var projectsScene = new ScrollMagic.Scene({
+	// 	triggerElement: '.projectsTitle',
+	// 	duration: '70%',
+	// 	triggerHook: "onEnter"
+
+	// })
+	// .setClassToggle('#project1', 'fade-in')
+	// .addIndicators()
+	// .addTo(controller);
+
+	var pinIntro = new ScrollMagic.Scene({
+
+		triggerElement: ".projects",
+		triggerHook: "onLeave",
+		duration: '300%'
+	})
+	.setPin('.projects')
+	.setTween(wipeAnimation)
+	.addIndicators({
+		name: 'start pin 2',
+		colorTrigger: 'red',
+		colorStart: '#75C695',
+		colorEnd: 'green'
+	})
 	.addTo(controller);
 
 });
