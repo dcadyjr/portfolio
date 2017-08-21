e2a7c89f7a13443147ca394fb160484f7c50a7ca
David Cady Jr <davidcadyjr@mac.com>
2017-08-21 15:26:27 -0500

    scrolling for projects is working correctly

--- js/main.js
+++ js/main.js
@@ -3,35 +3,11 @@ $(document).ready(function(){
 	var controller = new ScrollMagic.Controller();
 
 	var wipeAnimation = new TimelineMax()
-		.fromTo('#project1', 10, {x: '-100%'}, {x: "0%", ease: Linear.easeNone})
-		.fromTo('#project2', 10, {x: '-100%'}, {x: "0%", ease: Linear.easeNone})
-		.fromTo('#project3', 10, {x: '-100%'}, {x: "0%", ease: Linear.easeNone});
+		.fromTo('#project1', 100, {x: '-100%'}, {x: "100%", ease: Linear.easeNone})
+		.fromTo('#project2', 100, {x: '-100%'}, {x: "100%", ease: Linear.easeNone})
+		.fromTo('#project3', 100, {x: '-100%'}, {x: "100%", ease: Linear.easeNone});
 
 
-	// var firstScene = new ScrollMagic.Scene({
-	// 	triggerElement: '.intro',
-	// 	duration: '10%',
-	// 	triggerHook: "onEnter"
-	// })
-	// .setClassToggle('.projects', 'fade-in')
-	// .addIndicators({
-	// 	name: 'fade scene 1',
-	// 	colorTrigger: 'black',
-	// 	colorStart: '#75C695',
-	// 	colorEnd: 'pink'
-	// })
-	// .addTo(controller);
-
-
-	// var projectsScene = new ScrollMagic.Scene({
-	// 	triggerElement: '.projectsTitle',
-	// 	duration: '70%',
-	// 	triggerHook: "onEnter"
-
-	// })
-	// .setClassToggle('#project1', 'fade-in')
-	// .addIndicators()
-	// .addTo(controller);
 
 	var pinIntro = new ScrollMagic.Scene({
 
@@ -39,6 +15,7 @@ $(document).ready(function(){
 		triggerHook: "onLeave",
 		duration: '300%'
 	})
+
 	.setPin('.projects')
 	.setTween(wipeAnimation)
 	.addIndicators({
